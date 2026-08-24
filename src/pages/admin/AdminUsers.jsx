aeccae70e1586f7365684c9eca1acc43084c5e16
Yahaya Abdulbari <yahayaabdulbari13@gmail.com>
2026-08-24 03:48:40 +0100

    Update AdminUsers.jsx

--- src/pages/admin/AdminUsers.jsx
+++ src/pages/admin/AdminUsers.jsx
@@ -1,14 +1,11 @@
 // src/pages/admin/AdminUsers.jsx
 import React, { useState, useEffect } from 'react';
-import {
-  Users,
-  Search,
+import { 
+  Users, 
+  Search, 
   Edit,
-  Trash2,
   Shield,
-  Mail,
   Phone,
-  Calendar,
   MapPin,
   CheckCircle,
   XCircle,
@@ -19,7 +16,7 @@ import {
   ChevronRight,
   DollarSign,
   TrendingUp,
-  User,
+  User
 } from 'lucide-react';
 import { toast } from 'react-toastify';
 import axiosInstance from '../../utils/axios';
